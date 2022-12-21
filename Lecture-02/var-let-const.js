f0571eea8e21c7b0dc0e541f251624ec3e72d668
Your Name <you@example.com>
2022-12-21 09:44:51 +0700

    updeat

---
+++ Lecture-02/var-let-const.js
@@ -0,0 +1,16 @@
+var hello = "Hello";
+
+console.log(hello);
+hello = "Hello World";
+console.log(hello);
+
+if (true) {
+    let World ="Hello World";
+    console.let(World);
+}
+ //console.log(World);
+
+ const fixval = "Fox Value";
+ console.log(fixval);
+
+    //fixval = "tes assigne value to const"
