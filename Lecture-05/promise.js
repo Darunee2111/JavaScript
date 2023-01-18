f03ec5a9520e05cb81d2d6021c424fa5615c040c
Your Name <you@example.com>
2023-01-18 09:55:59 +0700

    update

--- Lecture-05/promise.js
+++ Lecture-05/promise.js
@@ -1,21 +1,17 @@
-function promiseTimrout(ms) {
+function promiseTimeout(ms) {
     return new Promise((resolve, reject) => {
         setTimeout(resolve, ms);
     });
 }
-
+console.log('Start');
 promiseTimeout(2000)
     .then(() => {
         console.log("Done!!");
-        return promiseTimeout(1000);
-    })
-    .then(() => {
-        console.log("Also done!!");
-        return Promise.resolve(42);
-    })
-    .then((result) => {
-        console.log(result);
+        return promiseTimeout(3000);
     })
     .catch(() => {
-        console.log("Error!")
+        console.log("Error!");
     });
+    console.log('End');
+
+    
